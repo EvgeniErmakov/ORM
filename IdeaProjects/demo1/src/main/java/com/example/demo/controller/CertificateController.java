b0f76d94b366e1665660ddcfc864a66a9ea1daf5
Evgeni Ermakov <Evgeni.Ermakov@artezio.com>
2021-12-05 16:13:24 +0300

    feat: add merge method

--- IdeaProjects/demo1/src/main/java/com/example/demo/controller/CertificateController.java
+++ IdeaProjects/demo1/src/main/java/com/example/demo/controller/CertificateController.java
@@ -2,21 +2,26 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Comment;
 import com.example.demo.model.Topic;
-import com.example.demo.repository.JpaTestCommands;
+import com.example.demo.repository.JpaCommentCommands;
+import com.example.demo.repository.JpaTopicCommands;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/certificates")
+@RequestMapping(value = "/topic")
 @AllArgsConstructor
 @Validated
 public class CertificateController {
 @Autowired
-private final JpaTestCommands topicRepository;
+private final JpaTopicCommands topicRepository;
+
+@Autowired
+private final JpaCommentCommands commentRepository;
 
     @GetMapping(value = "/{id}")
     public void findById() {
@@ -29,4 +34,17 @@ private final JpaTestCommands topicRepository;
 
         System.out.println("lolz");
     }
+
+    @GetMapping(value = "/merge/{id}")
+    public void findById(@PathVariable Long id) {
+        Topic topic = topicRepository.getOne(id);
+        topic.setTitle("Updated Title");
+
+        Comment comment = commentRepository.getOne(14L);
+        comment.setText("Updated Text");
+        topicRepository.save(topic);
+
+        System.out.println("lolz2");
+    }
+
 }
--- IdeaProjects/demo1/src/main/java/com/example/demo/repository/JpaTopicCommands.java
+++ IdeaProjects/demo1/src/main/java/com/example/demo/repository/JpaTopicCommands.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JpaTestCommands extends JpaRepository<Topic, Integer> {
+public interface JpaTopicCommands extends JpaRepository<Topic, Long> {
 
 }
